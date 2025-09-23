4a69532a7f45ed8e84bf61de47b37be5d42cb62d
Mariusz Kogen <k0gen@pm.me>
2025-09-23 07:18:23 +0200

    keep up with latest Makefile, sdk.alpha.41 and others

--- startos/manifest.ts
+++ startos/manifest.ts
@@ -1,4 +1,10 @@
 import { setupManifest } from '@start9labs/start-sdk'
+import { SDKImageInputSpec } from '@start9labs/start-sdk/base/lib/types/ManifestTypes'
+
+const BUILD = process.env.BUILD || ''
+
+const architectures =
+  BUILD === 'x86_64' || BUILD === 'aarch64' ? [BUILD] : ['x86_64', 'aarch64']
 
 export const manifest = setupManifest({
   id: 'nostrudel',
@@ -21,9 +27,10 @@ export const manifest = setupManifest({
       source: {
         dockerTag: 'ghcr.io/hzrd149/nostrudel:0.45.0',
       },
-    },
+      arch: architectures
+    } as SDKImageInputSpec,
   },
-  hardwareRequirements: {},
+  hardwareRequirements: { arch: architectures },
   alerts: {
     install: null,
     update: null,
