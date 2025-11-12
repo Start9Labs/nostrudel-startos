20c1b8630e614eebc8deb43302c9d484152ed8d2
Mariusz Kogen <k0gen@pm.me>
2025-11-12 15:01:09 +0100

    Misc updates
        - Renames interfaces for backward compatibility
        - Updates to sdk.beta.43
        - Fixes start-cli init-key issue
        - Updates devDependencies

--- startos/install/versions/v0.46.1.1-alpha.0.ts
+++ startos/install/versions/v0.46.1.1-alpha.0.ts
@@ -1,28 +1,29 @@
 import { IMPOSSIBLE, VersionInfo } from '@start9labs/start-sdk'
 
 export const v_0_46_1_1 = VersionInfo.of({
-  version: '0.46.1:1-alpha.0',
+  version: '0.46.1:1-alpha.1',
   releaseNotes: `\
-  ## Updated for StartOS 0.4.0
+## Updated for StartOS 0.4.0
 
-  ### Dependency Updates
-  * Updated SDK to beta.41
-  * Updated dev dependencies to latest versions
-  
-  ## 0.46.1
-  ### Patch Changes
-  * Fix issue with application hanging from capacitorjs modules
-  * Fix elements under nav bar on mobile
-  ## 0.46.0
-  ### Minor Changes
-  * Add option for custom share service (njump.me)
-  * Add article reader component using native web TTS API
-  * Show favorite relays in discovery view
-  * Add relay feed to discovery view
-  * Redesign user profile navigation
-  ### Patch Changes
-  * Fix open graph link images not being blured
-  * Fix ncrypsec accounts not prompting for decryption password`,
+### Dependency Updates
+* Updated SDK to beta.43
+* Updated interface name for backwards compatibility
+* Updated dev dependencies to latest versions
+
+## 0.46.1
+### Patch Changes
+* Fix issue with application hanging from capacitorjs modules
+* Fix elements under nav bar on mobile
+## 0.46.0
+### Minor Changes
+* Add option for custom share service (njump.me)
+* Add article reader component using native web TTS API
+* Show favorite relays in discovery view
+* Add relay feed to discovery view
+* Redesign user profile navigation
+### Patch Changes
+* Fix open graph link images not being blured
+* Fix ncrypsec accounts not prompting for decryption password`,
   migrations: {
     up: async () => {},
     down: IMPOSSIBLE,
